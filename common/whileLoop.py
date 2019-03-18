d5491b3b303777dd85f0985c2f3433aec62a3c74
chulman <chlcjfals0122@gmail.com>
2019-03-18 23:36:38 +0900

    til - flow control, random, zip, setdefault

--- common/whileLoop.py
+++ common/whileLoop.py
@@ -12,4 +12,30 @@ i=0
 while i<3:
     print(i)
     i+=1
-   
+
+############## sequence
+# 1.zip
+days = 'monday', 'tuesday', 'wendsday'
+french = 'a', 'b', 'c', 'd'
+
+zip_list = list(zip(days, french))
+
+print(zip_list)
+
+# 2.range
+
+for x in range(0, 3):
+    print(x)
+
+
+for x in range(2, -1, -1):
+    print(x)
+
+
+print(list(range(2010, 2016, 2)))
+
+dicts = {"1":"asd"}
+dicts.setdefault("defalut", "kkk")
+
+print(dicts)
+
